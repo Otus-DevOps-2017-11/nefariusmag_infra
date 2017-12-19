8ca12aaeb6f6db8da5ac9dcf16b72200c876d812
Dmitriy Erokhin <nefariusmag@gmail.com>
2017-12-19 14:59:30 +0300

    Edit script for install

--- Startup.sh
+++ Startup.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y ruby-full ruby-bundler build-essential
+user=reddit
+useradd ${user}
+mkdir /home/${user}
+chown -R ${user}:${user} /home/${user}
+cd /home/${user}
+
+apt update -y
+apt install -y ruby-full ruby-bundler build-essential
 if [ "$(ruby -v)" == 'ruby 2.3.1p112 (2016-04-26) [x86_64-linux-gnu]' ]; then 
-	echo "true install ruby"
+	echo "true install ruby" > install.log
 else 
-	echo "false install ruby" 
+	echo "false install ruby" > install.log
 fi
 if [ "$(bundle -v)" == 'Bundler version 1.11.2' ]; then
-        echo "true install bundler" > install.log
+        echo "true install bundler" >> install.log
 else
-        echo "false install bundler" > install.log
+        echo "false install bundler" >> install.log
 fi
+
 sleep 2
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-sudo apt update
-sudo apt install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt update
+apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
 if [ "$(sudo systemctl status mongod) | wc -l" == "9" ]; then
 	echo "true install & start mongod" >> install.log
 else
 	echo "false install & start mongod" >> install.log
 fi
+
 sleep 2
-cd ~
+
 git clone https://github.com/Otus-DevOps-2017-11/reddit.git
 cd reddit && bundle install
 bundle install
-puma -d
+su -l ${user} -c "puma -d"
 if [ "$(ps aux | grep puma | grep -v grep | wc -l)" == "1" ]; then
 	echo "true install puma" >> install.log
 else
