7d091c12a99870e98894eda67eb9d73e45c58b40
Dmitriy Erokhin <nefariusmag@gmail.com>
2017-12-19 15:31:37 +0300

    Edit test in Startup.sh part 4

--- Startup.sh
+++ Startup.sh
@@ -2,20 +2,19 @@
 user=reddit
 useradd ${user}
 mkdir /home/${user}
-chown -R ${user}:${user} /home/${user}
 cd /home/${user}
 
 apt update -y
 apt install -y ruby-full ruby-bundler build-essential
 if [ "$(ruby -v)" == 'ruby 2.3.1p112 (2016-04-26) [x86_64-linux-gnu]' ]; then 
-	echo "true install ruby" > install.log
+	echo "true install ruby" > /home/${user}/install.log
 else 
-	echo "false install ruby" > install.log
+	echo "false install ruby" > /home/${user}/install.log
 fi
 if [ "$(bundle -v)" == 'Bundler version 1.11.2' ]; then
-        echo "true install bundler" >> install.log
+        echo "true install bundler" >> /home/${user}/install.log
 else
-        echo "false install bundler" >> install.log
+        echo "false install bundler" >> /home/${user}/install.log
 fi
 
 sleep 2
@@ -27,19 +26,19 @@ apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
 if [ -n "$(systemctl status mongod | grep running)" ]; then
-	echo "true install & start mongod" >> install.log
+	echo "true install & start mongod" >> /home/${user}/install.log
 else
-	echo "false install & start mongod" >> install.log
+	echo "false install & start mongod" >> /home/${user}/install.log
 fi
 
 sleep 2
 
 git clone https://github.com/Otus-DevOps-2017-11/reddit.git
 cd reddit && bundle install
-bundle install
-su -l ${user} -c "puma -d"
+chown -R ${user}:${user} /home/${user}
+sudo -u ${user} bash -c "puma -d"
 if [ "$(ps aux | grep puma | grep -v grep | wc -l)" == "1" ]; then
-	echo "true install puma" >> install.log
+	echo "true install puma" >> /home/${user}/install.log
 else
-	echo "false install puma" >> install.log
+	echo "false install puma" >> /home/${user}/install.log
 fi
