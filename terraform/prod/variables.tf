7db194f6006b6e84608a88557d202270f0fb44ef
Dima <nefariusmag@gmail.com>
2018-01-04 12:50:03 +0300

    Добавил модули для бд, приложения и vpc + удаленное хранение стейт файла

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -19,11 +19,21 @@ variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
 
-variable zona {
-  description = "Zona"
+variable zone {
+  description = "Zone"
   default     = "europe-west1-b"
 }
 
 variable user {
   description = "Name user"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
---
+++ terraform/stage/main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "reddit-terraform"
+    prefix  = "stage"
+  }
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
+  user            = "${var.user}"
+}
+
+module "db" {
+  source          = "../modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.db_disk_image}"
+  user            = "${var.user}"
+}
+
+module "vpc" {
+  source        = "../modules/vpc"
+  source_ranges = ["0.0.0.0/0"]
+}
